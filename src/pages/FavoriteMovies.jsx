2df84e3d561a801475730cc994e6a4073a4a5c80
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-05-27 14:29:17 +0530

    ADDED

--- src/pages/FavoriteMovies.jsx
+++ src/pages/FavoriteMovies.jsx
@@ -6,7 +6,7 @@ const FavoriteMovies = () => {
   const favoriteMovies = useSelector((state) => state.movies.favoriteMovies);
 
   return (
-    <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className=" p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {favoriteMovies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
