fe4ae522c4f6b9cfa9046fa267cf7c7fe8be11e7
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-05-29 08:13:12 +0530

    Done

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -32,7 +32,7 @@ const MovieList = () => {
   }
 
   return (
-    <div className="p-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="p-16 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {content}
     </div>
   );
